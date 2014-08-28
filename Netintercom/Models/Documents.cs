374e4cba53c7868e24239ac338ea97cab28f0330
Icecoder1 <ice1@me.com>
2014-08-28 14:47:20 +0200

    Added Document Model

---
+++ Netintercom/Models/Documents.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Netintercom.Models
+{
+    public class Documents
+    {
+        public int DocId { get; set; }
+        public string DocumentName { get; set; }
+        public string PathUrl { get; set; }
+    }
+}
